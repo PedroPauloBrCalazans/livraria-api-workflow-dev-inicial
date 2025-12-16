3f2a3c32382897e69cf90b7ce24fcaeb85e45578
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2025-12-15 23:58:53 -0300

    feat: Retirando implementação do endpoint de listar livros por autor.

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -6,7 +6,6 @@ const router = express.Router();
 router
   .get("/autores", AutoresController.listarAutores)
   .get("/autores/:id", AutoresController.listarAutorPorId)
-  .get("/autores/:id/livros", AutoresController.listaLivrosPorAutor)
   .post("/autores", AutoresController.cadastrarAutor)
   .put("/autores/:id", AutoresController.atualizarAutor)
   .delete("/autores/:id", AutoresController.excluirAutor);
