a594c1e03149c14f26e6e5b79dc3cefb942ecd72
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-01-05 23:44:08 -0300

    feat: Add model eventos + teste do modelo.

---
+++ src/models/evento.js
@@ -0,0 +1,31 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable camelcase */
+// import db from "../db/dbconfig.js";
+
+class Evento {
+  constructor({ id, nome, descricao, data, autor_id, created_at, updated_at }) {
+    this.id = null || id;
+    this.nome = nome;
+    this.descricao = descricao;
+    this.data = data;
+    this.autor_id = autor_id;
+    this.created_at = created_at || new Date().toISOString();
+    this.updated_at = updated_at || new Date().toISOString();
+  }
+
+  static async pegarEvento() {
+    return [
+      {
+        id: 1,
+        nome: "lançamento",
+        descricao: "descrição",
+        data: "2026-01-26",
+        autor_id: 1,
+        created_at: "2026-01-26 07:00:00",
+        updated_at: "2026-01-26 07:00:00",
+      },
+    ];
+  }
+}
+
+export default Evento;
